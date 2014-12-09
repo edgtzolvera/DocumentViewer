df155143774731c7afe42c8fa2f14e5ba2411866
Eduardo Gutiérrez Olvera <edgtzolvera@gmail.com>
2014-12-09 12:11:09 -0600

    - Adding audit document and unit test for the same.

---
+++ DocumentViewer/grails-app/domain/com/qualcomm/demo/AuditDocument.groovy
@@ -0,0 +1,24 @@
+package com.qualcomm.demo
+
+import groovy.transform.EqualsAndHashCode
+
+
+@EqualsAndHashCode
+class AuditDocument extends AbstractDocument implements Serializable {
+
+    long id
+    Date fromDate
+    Date toDate
+    long version
+
+    static mapping = {
+        version bindable: true
+        id composite: ['id', 'fromDate'], bindable: true
+    }
+
+    static constraints = {
+        //not really necessary as they are non-nullable by default, but here for readability and domain documentation
+        fromDate nullable: false
+        toDate nullable: false
+    }
+}
---
+++ DocumentViewer/test/unit/com/qualcomm/demo/AuditDocumentSpec.groovy
@@ -0,0 +1,37 @@
+package com.qualcomm.demo
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(AuditDocument)
+class AuditDocumentSpec extends Specification {
+
+    @Unroll("Test Audit Document constraints for author: #author, description: #description, pubDate: #pubDate")
+    void "Parametrized constraints test for the auditable document domain class"() {
+        given:
+        AuditDocument document = new AuditDocument(author: author,
+                description: description,
+                pubDate: pubDate,
+                fromDate: fromDate,
+                toDate: toDate)
+
+        expect:
+        document.validate() == shouldValidate
+        document.errors?.errorCount  == errorCount
+
+        where:
+        author      | description   |   pubDate     | fromDate  | toDate    || shouldValidate | errorCount
+        ""          |   ""          |   null        | null      | null      ||   false        | 5
+        null        |   null        |   null        |new Date() |new Date() ||   false        | 3
+        ""          |   "D"         |   null        |new Date() |new Date() ||   false        | 2
+        "A"         |   "D"         | new Date()    |new Date() |new Date() ||   true         | 0
+        "A" * 255   |   "D"         | new Date()    |new Date() |new Date() ||   true         | 0
+        "A" * 256   |   "D"         | new Date()    |new Date() |new Date() ||   false        | 1
+        "A"         |   "D" * 2000  | new Date()    |new Date() |new Date() ||   true         | 0
+        "A"         |   "D" * 2001  | new Date()    |new Date() |new Date() ||   false        | 1
+    }
+}

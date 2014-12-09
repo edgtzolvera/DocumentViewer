71d75084025697530acfa8135298f38fbf59eaec
Eduardo Gutiérrez Olvera <edgtzolvera@gmail.com>
2014-12-09 11:50:07 -0600

    - Adding data binders

--- DocumentViewer/grails-app/conf/Config.groovy
+++ DocumentViewer/grails-app/conf/Config.groovy
@@ -11,7 +11,7 @@
 //    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
 // }
 
-grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
+grails.project.groupId = "com.qualcomm.demo" // change this to alter the default package name and Maven publishing destination
 
 // The ACCEPT header will not be used for content negotiation for user agents containing the following strings (defaults to the 4 major rendering engines)
 grails.mime.disable.accept.header.userAgents = ['Gecko', 'WebKit', 'Presto', 'Trident']
@@ -115,3 +115,7 @@ log4j.main = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+
+//Adding additional date binders to correspond with the date objects typically sent by JS apps
+grails.databinding.dateFormats = ["yyyy-MM-dd'T'HH:mm:ss.SSSXXX", "yyyy-MM-dd HH:mm:ss.S", "yyyy-MM-dd'T'hh:mm:ss'Z'"]
